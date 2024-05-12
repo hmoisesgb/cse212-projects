7ccf65191bebcb1e92afbf73eba6653b4d04d3d4
hmoisesgb <h.moises.gb@gmail.com>
2024-05-12 02:25:01 -0600

    Updated problem 5

--- week03/code/SetsAndMapsTester.cs
+++ week03/code/SetsAndMapsTester.cs
@@ -310,13 +310,13 @@ public static class SetsAndMapsTester {
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
-        foreach (var feature in featureCollection.features) {
-            Console.WriteLine($"{feature.properties.place} - {feature.properties.mag}");
-        }
-
         // TODO:
         // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to print out each place a earthquake has happened today and its magitude.
+
+        foreach (var feature in featureCollection.features) {
+            Console.WriteLine($"{feature.properties.place} - Mag: {feature.properties.mag}");
+        }
     }
 }
